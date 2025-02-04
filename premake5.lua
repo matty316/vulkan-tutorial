4760d10c4c78129ba1c51fcdc84c3a48b99d16ca
matty <mattreed1890@gmail.com>
2025-02-04 14:06:50 -0600

    add libs

--- premake5.lua
+++ premake5.lua
@@ -1,17 +1,21 @@
-workspace "WorkspaceName"
+workspace "VulkanTutorial"
     configurations { "Debug", "Release" }
     platforms { "Win64" }
 
-project "ProjectName"
+project "VulkanTutorial"
     kind "ConsoleApp"
     language "C++"
     targetdir "bin/%{cfg.buildcfg}"
     cppdialect "C++23"
 
-    files { "include/**.h", "src/**.c", "src/**.cpp" }
+    files { "include/**.h", "include/**.hpp", "src/**.c", "src/**.cpp" }
 
     includedirs { "include" }
     includedirs { "src" }
+    libdirs { "lib" }
+
+    links { "glfw3" }
+    links { "vulkan-1.lib" }
 
     filter "configurations:Debug"
     defines { "DEBUG" }
